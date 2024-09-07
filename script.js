55f0f64ffb9926283b628f9cae7d1c7c076a8394
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2024-09-07 13:33:18 -0500

    add book constructor

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const myLibrary = [];
 
-function Book(){
-
+function Book(title, author, pages, read_status){
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.read_status = read_status;
 }
 
 function addBookToLibrary(){
